b2f5252b50a3d11e00efc32f16c50ded3202187f
Yuna Nohara <e195747@ie.u-ryukyu.ac.jp>
2019-11-25 13:28:30 +0900

    1st commit

--- src/main/jp/ac/uryukyu/ie/e195747/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e195747/Enemy.java
@@ -47,9 +47,12 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
-    }
+        if(!dead){
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+            hero.wounded(damage);
+        }
+        }
+
 
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
